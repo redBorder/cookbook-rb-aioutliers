10d5e95740b919de49f1f752db87211e681b0711
malvads <malvarez@redborder.com>
2024-01-25 17:15:11 +0000

    Fix attributes

--- resources/attributes/default.rb
+++ resources/attributes/default.rb
@@ -1,6 +1,3 @@
-# Cookbook:: :: rbaioutliers
+# default attributes
 #
-# Resource:: config
-attribute :druid_broker, kind_of: String, default: 'druid-broker.service:8082'
-attribute :log_file, kind_of: String, default: '/var/log/rb-aioutliers/outliers.log'
-attribute :s3_hostname, kind_of: String, default: 's3.service:9000'
+default['rb-aioutliers']['registered'] = false
